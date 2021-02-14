73dd905ef67d60bcddca64f889029326f458f054
Hyeonyeong Kim <hyeonyeongkim@Hyeonyeongui-MacBookPro.local>
2021-02-14 16:09:45 +0900

    refactored with express

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,16 +1,3 @@
-// const http = require("http"); // node core module인 http module 사용
-// const requestHandler = require('./request-handler.js') // 직접 작성한 request-handler.js 파일을 사용하도록 하는 file module
-
-// const port = 3000;
-// const ip = "127.0.0.1";
-
-// const server = http.createServer(requestHandler);
-// console.log(`Listening on http://${ip}:${port}`);
-
-// server.listen(port, ip);
-
-// module.exports = server;
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); //npm install body-parser 후 사용할 수 있다.
@@ -37,3 +24,4 @@ app.listen(port, () => {
 
 module.exports = app;
 
+//  request handler not required.
